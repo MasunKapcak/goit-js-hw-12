1f8c29c791896eb04ddc5f24c631f21448a0a2cf
masun <masunkpck@gmail.com>
2024-11-03 15:51:03 +0300

    final edit

--- src/main.js
+++ src/main.js
@@ -2,28 +2,41 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import axios from 'axios';
 
 const input = document.querySelector('.search-input');
-const searchBtn = document.querySelector('.search-btn');
 const gallery = document.getElementById('gallery');
 const form = document.querySelector('.search-form');
-
 const lightbox = new SimpleLightbox('.gallery li > a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
+const loadMoreBtn = document.getElementById('load-more');
+
+let page = 1;
+let currentQ = '';
 
 async function searchImages() {
-  const loader = document.getElementById('loader');
-  const q = input.value;
+  const q = input.value.trim();
+  let loader;
+
+  if (q !== '' && q !== currentQ) {
+    page = 1;
+    currentQ = q;
+    gallery.innerHTML = '';
+    loader = document.getElementById('loader-container');
+  } else {
+    loader = document.getElementById('loader-more-container');
+  }
+
   const apiKey = '46766267-b6642edcf68e61aa5f54d39e5';
-  const url = `https://pixabay.com/api/?key=${apiKey}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true`;
+  const url = `https://pixabay.com/api/?key=${apiKey}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`;
 
   try {
-    gallery.innerHTML = '';
     loader.classList.remove('hidden');
-    const response = await fetch(url);
-    const data = await response.json();
+
+    const response = await axios.get(url);
+    const data = response.data;
 
     if (data.hits.length === 0) {
       iziToast.error({
@@ -34,9 +47,16 @@ async function searchImages() {
       });
     } else {
       displayImages(data.hits);
+      loadMoreBtn.classList.remove('hidden');
+      page++;
     }
   } catch (error) {
-    console.error('Hata:', error);
+    iziToast.error({
+      title: '',
+      message: `Sorry, ${error.message}! Please try again!`,
+      position: 'topRight',
+    });
+    console.error(error);
   } finally {
     loader.classList.add('hidden');
   }
@@ -46,26 +66,24 @@ function displayImages(images) {
   const galleryHtml = images.map(image => {
     return `
     <li class="card">
-  <a href="${image.largeImageURL}">
-    <img src="${image.webformatURL}" alt="${image.tags}" />
-  </a>
-  <div class="info">
-    <p class="info-text"><b>Likes</b> ${image.likes}</p>
-    <p class="info-text"><b>Views</b> ${image.views}</p>
-    <p class="info-text"><b>Comments</b> ${image.comments}</p>
-    <p class="info-text"><b>Downloads</b> ${image.downloads}</p>
-  </div>
-</li>
-    
+      <a href="${image.largeImageURL}">
+        <img src="${image.webformatURL}" alt="${image.tags}" />
+      </a>
+      <div class="info">
+        <p class="info-text"><b>Likes</b> ${image.likes}</p>
+        <p class="info-text"><b>Views</b> ${image.views}</p>
+        <p class="info-text"><b>Comments</b> ${image.comments}</p>
+        <p class="info-text"><b>Downloads</b> ${image.downloads}</p>
+      </div>
+    </li> 
     `;
   });
 
-  gallery.innerHTML = galleryHtml.join('');
+  gallery.innerHTML += galleryHtml.join('');
   lightbox.refresh();
 }
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-
   searchImages();
 });
